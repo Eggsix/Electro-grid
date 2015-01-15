deb3478670e74d7ec185ae274f51800eabb2b5c5
Preston <plphan206@hotmail.com>
2015-01-15 13:21:32 -0800

    Grid with new grid option

--- jquery.js
+++ jquery.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	$('body').prepend('<div class="container"></div>');
+	$('.container').prepend('<button onclick="newGrid(); return false">New Grid</button>');
 		for (i = 0; i < 16; ++i){
 			$('.container').append('<tr></tr>');
 		}
@@ -12,5 +13,30 @@ $(document).ready(function(){
 		border: '1px black solid',
 		margin: '0 1px',
 		'box-sizing': 'border-box'
+	}).hover(function(){
+		$(this).css('background-color', 'red');
 	});
 });
+function newGrid() {
+	$('.container').remove();
+	$('body').append('<div class="container"></div>');
+	$('.container').prepend('<button onclick="newGrid(); return false">New Grid</button>');
+
+		var height = prompt("What height?");
+		var width = prompt("What width?")
+		for (i = 0; i < height; ++i){
+			$('.container').append('<tr></tr>');
+		}
+		for (j = 0; j < width; ++j){
+			$('tr').append('<td></td>')
+		}
+		$('td').css({
+		height: '20px',
+		width: '20px',
+		border: '1px black solid',
+		margin: '0 1px',
+		'box-sizing': 'border-box'
+	}).hover(function(){
+		$(this).css('background-color', 'red');
+	});
+};
